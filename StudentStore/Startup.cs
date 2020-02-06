7c39c5365abb57ee592a71c5cc8f05fcdf976124
andrew7771 <aturyansky33@gmail.com>
2020-02-06 14:48:12 +0400

    added swagger

--- StudentStore/Startup.cs
+++ StudentStore/Startup.cs
@@ -11,6 +11,7 @@ using StudentStore.Repositories.Implementation;
 using StudentStore.Repositories.Interfaces;
 using StudentStore.Services.Implementation;
 using StudentStore.Services.Interfaces;
+using Microsoft.OpenApi.Models;
 
 namespace StudentStore
 {
@@ -19,6 +20,12 @@ namespace StudentStore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+
+
             services.AddTransient<IStudentRepository, StudentRepository>();
             services.AddTransient<IStudentService, StudentService>();
         }
@@ -29,6 +36,12 @@ namespace StudentStore
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseRouting();
 
