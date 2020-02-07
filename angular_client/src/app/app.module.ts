7ad8b677a48da03effcd1cd5327054268deacd45
andrew7771 <aturyansky33@gmail.com>
2020-02-07 15:54:48 +0400

    added new boilerplate for angular with webpack

--- angular_client/src/app/app.module.ts
+++ angular_client/src/app/app.module.ts
@@ -1,10 +1,26 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { AppComponent }   from './app.component';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './shared/components/home/home.component';
+
+import '../assets/styles';
+
 @NgModule({
-    imports:      [ BrowserModule, FormsModule ],
-    declarations: [ AppComponent ],
-    bootstrap:    [ AppComponent ]
+    declarations: [
+        AppComponent,
+        HomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [
+        AppComponent
+    ]
 })
-export class AppModule { }
+
+export class AppModule {}
--- angular_client/webpack.config.js
+++ angular_client/webpack.config.js
@@ -1,37 +1,9 @@
-const path = require('path');
-const webpack = require('webpack');
-module.exports = {
-    entry: {
-        'polyfills': './src/polyfills.ts',
-        'app': './src/main.ts'
-      },
-   output:{
-       path: path.resolve(__dirname, './public'),     // путь к каталогу выходных файлов - папка public
-       publicPath: '/public/',
-       filename: "[name].js"       // название создаваемого файла
-   },
-   resolve: {
-    extensions: ['.ts', '.js']
-  },
-   module:{
-       rules:[   //загрузчик для ts
-           {
-               test: /\.ts$/, // определяем тип файлов
-               use: [
-                {
-                    loader: 'awesome-typescript-loader',
-                    options: { configFileName: path.resolve(__dirname, 'tsconfig.json') }
-                  } ,
-                   'angular2-template-loader'
-               ]
-            }
-       ]
-   },
-   plugins: [
-    new webpack.ContextReplacementPlugin(
-        /angular(\\|\/)core/,
-        path.resolve(__dirname, 'src'), // каталог с исходными файлами
-      {} // карта маршрутов
-    )
-  ]
+'use strict';
+
+const environment = (process.env.NODE_ENV || 'development').trim();
+
+if (environment === 'development') {
+    module.exports = require('./config/webpack.config.dev');
+} else {
+    module.exports = require('./config/webpack.config.prod');
 }
