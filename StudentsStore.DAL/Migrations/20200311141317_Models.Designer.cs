9a2574b8d52c89955b1593815cab79deea60102b
andrew7771 <aturyansky33@gmail.com>
2020-03-11 18:18:46 +0400

    added migrations

--- StudentsStore.DAL/Migrations/20200311141317_Models.Designer.cs
+++ StudentsStore.DAL/Migrations/20200311141317_Models.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StudentStore.DAL;
 
 namespace StudentStore.DAL.Migrations
 {
     [DbContext(typeof(StudentStoreContext))]
-    partial class StudentStoreContextModelSnapshot : ModelSnapshot
+    [Migration("20200311141317_Models")]
+    partial class Models
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ StudentsStore.DAL/Migrations/20200311141317_Models.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentStore.DAL.Migrations
+{
+    public partial class Models : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TeacherId",
+                table: "Subjects",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Teacher",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    MiddleName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    AcadDegree = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teacher", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubjectCp",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SubjectCpname = table.Column<string>(nullable: true),
+                    Term = table.Column<int>(nullable: false),
+                    TeacherId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubjectCp", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SubjectCp_Teacher_TeacherId",
+                        column: x => x.TeacherId,
+                        principalTable: "Teacher",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subjects_TeacherId",
+                table: "Subjects",
+                column: "TeacherId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubjectCp_TeacherId",
+                table: "SubjectCp",
+                column: "TeacherId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Subjects_Teacher_TeacherId",
+                table: "Subjects",
+                column: "TeacherId",
+                principalTable: "Teacher",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Subjects_Teacher_TeacherId",
+                table: "Subjects");
+
+            migrationBuilder.DropTable(
+                name: "SubjectCp");
+
+            migrationBuilder.DropTable(
+                name: "Teacher");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Subjects_TeacherId",
+                table: "Subjects");
+
+            migrationBuilder.DropColumn(
+                name: "TeacherId",
+                table: "Subjects");
+        }
+    }
+}
